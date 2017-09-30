915f31f1868644631788b528b47556a22a8a3563
Stingion <stingion@gmail.com>
2017-09-30 21:09:32 +0300

    Spring Security with hardcoded roles and user

--- mainside/src/main/java/com/stingion/makeitfine/controller/SecurityConfiguration.java
+++ mainside/src/main/java/com/stingion/makeitfine/controller/SecurityConfiguration.java
@@ -21,7 +21,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/login**", "/index**", "/public/**").permitAll();
-        http.authorizeRequests().antMatchers("/**").access("hasRole('ADMIN') or hasRole('USER')").and().formLogin();
+        http.authorizeRequests().antMatchers("/login**", "/public/**").permitAll();
+        http.authorizeRequests().antMatchers("/**").access("hasRole('ADMIN') or hasRole('USER')").and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("ssoId")
+                .passwordParameter("password")
+                .failureUrl("/Access_Denied");
+        http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/Access_Denied");
     }
 }
