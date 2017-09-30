470261552876215ad2435eafcf0a4bbc60140c18
Stingion <stingion@gmail.com>
2017-09-30 22:35:29 +0300

    flyway including

---
+++ mainside/src/main/resources/db/migration/mysql/V1_6__MySql_Change_User_State_From_Upper_Case.sql
@@ -0,0 +1,11 @@
+ALTER TABLE APP_USER DROP COLUMN STATE;
+ALTER TABLE APP_USER ADD STATE ENUM('Active', 'Inactive', 'Deleted', 'Locked');
+
+UPDATE APP_USER SET STATE = 'Active' where ID = 1;
+UPDATE APP_USER SET STATE = 'Active' where ID = 2;
+UPDATE APP_USER SET STATE = 'Inactive' where ID = 3;
+UPDATE APP_USER SET STATE = 'Locked' where ID = 4;
+UPDATE APP_USER SET STATE = 'Inactive' where ID = 5;
+UPDATE APP_USER SET STATE = 'Active' where ID = 6;
+UPDATE APP_USER SET STATE = 'Active' where ID = 7;
+UPDATE APP_USER SET STATE = 'Active' where ID = 8;
