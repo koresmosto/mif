f74f6af35e3ef543c027f10d16fbba29e58d8259
Stingion <stingion@gmail.com>
2017-10-02 20:55:04 +0300

    Add spring context

---
+++ mainside/src/main/java/com/stingion/makeitfine/configuration/ContextConfig.java
@@ -0,0 +1,11 @@
+package com.stingion.makeitfine.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:spring/context.xml")
+public class ContextConfig {
+    
+}
