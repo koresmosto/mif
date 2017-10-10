222f687be4246577d35fce7cdb94360865c3ea96
Stingion <stingion@gmail.com>
2017-10-10 20:03:30 +0300

    Include h2 with broken tests (these should be fixed)

---
+++ mainside/src/test/java/com/stingion/makeitfine/ItisTest.java
@@ -0,0 +1,26 @@
+package com.stingion.makeitfine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class ItisTest {
+    @Autowired
+    private JdbcTemplate template;
+
+    @Test
+    public void testDefaultSettings() throws Exception {
+        assertThat(this.template).isNotNull();
+        assertThat(this.template.queryForObject("SELECT COUNT(*) from BANK",
+                Integer.class)).isEqualTo(0);
+    }
+}
