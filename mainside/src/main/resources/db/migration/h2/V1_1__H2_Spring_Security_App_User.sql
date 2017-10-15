68ddfb88b2616f30e58a2908664182b5c64b2062
Stingion <stingion@gmail.com>
2017-10-15 13:35:02 +0300

    Add h2 flyway migration scripts with fix to 1.8 mysql migration script

---
+++ mainside/src/main/resources/db/migration/h2/V1_1__H2_Spring_Security_App_User.sql
@@ -0,0 +1,24 @@
+-- All User's are stored in APP_USER table
+CREATE TABLE APP_USER (
+  ID         INT          NOT NULL,
+  SSO_ID     VARCHAR(30)  NOT NULL UNIQUE,
+  PASSWORD   VARCHAR(100) NOT NULL,
+  FIRST_NAME VARCHAR(30)  NOT NULL,
+  LAST_NAME  VARCHAR(30)  NOT NULL,
+  EMAIL      VARCHAR(30)  NOT NULL UNIQUE,
+  STATE      VARCHAR(30)  NOT NULL,
+  PRIMARY KEY (ID)
+);
+
+-- Populate APP_USER Table
+INSERT INTO APP_USER VALUES (1, 'bill', 'abc123', 'Bill', 'Watcher', 'bill@xyz.com', 'Active');
+INSERT INTO APP_USER VALUES (2, 'danny', 'abc124', 'Danny', 'Theys', 'danny@xyz.com', 'Active');
+INSERT INTO APP_USER VALUES (3, 'sam', 'abc125', 'Sam', 'Smith', 'samy@xyz.com', 'Active');
+INSERT INTO APP_USER VALUES (4, 'nicole', 'abc126', 'Nicole', 'warner', 'nicloe@xyz.com', 'Active');
+INSERT INTO APP_USER VALUES (5, 'kenny', 'abc127', 'Kenny', 'Roger', 'kenny@xyz.com', 'Active');
+INSERT INTO APP_USER VALUES (6, 'sudo', 'nimda', 'SUDO', 'SUDO', 'sudo@xxx.xxx', 'Active');
+INSERT INTO APP_USER VALUES (7, 'tsuser', 'tspass1', 'Testside', 'User', 'tsuser@xxx.xxx', 'Active');
+INSERT INTO APP_USER VALUES (8, 'admin', 'nimda', 'Admin', 'User', 'admin@xxx.xxx', 'Active');
+
+-- Should be inserted next generated value (9)
+INSERT INTO SEQUENCES VALUES ('APP_USER_SEQUENCE', 9);
