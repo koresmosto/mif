8ac0ba5197d645ee4283f6d2a492550011db21a8
Stingion <stingion@gmail.com>
2017-10-20 09:25:56 +0300

    Begining creating unit tests for DB services (user)

--- mainside/src/test/java/com/stingion/makeitfine/data/service/EntityServiceTestAncestor.java
+++ mainside/src/test/java/com/stingion/makeitfine/data/service/EntityServiceTestAncestor.java
@@ -1,13 +1,17 @@
 package com.stingion.makeitfine.data.service;
 
 import com.stingion.makeitfine.ItIsTestConfiguration;
+import com.stingion.makeitfine.ServiceTestConfiguration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,9 +23,23 @@ public class EntityServiceTestAncestor<T> {
     protected EntityService<T> entityService;
 
     @Autowired
-    protected String any;
+    protected ServiceTestConfiguration.EntityTestData<T> entityTestData;
 
     @Test
-    public void findAll() {
+    public void findAllTest() {
+        assertEquals(entityTestData.getFindAll(), entityService.findAll().toString());
+    }
+
+    @Test
+    public void findByIdTest() {
+        assertEquals(entityTestData.getFindById(), entityService.findById(entityTestData.getId()).toString());
+    }
+
+    @Test
+    @Rollback//todo:solve issue with rollback after db changes in tests
+    public void deleteTest() {
+        T entity = entityService.findById(entityTestData.getId());
+        entityService.delete(entity);
+        assertEquals(entityTestData.getDelete(), entityService.findAll().toString());
     }
 }
