4f02594e5a00815be12fb9386b8f07fd57c49077
Stingion <stingion@gmail.com>
2017-10-31 23:18:56 +0200

    Fix test with deleting in bank

--- mainside/src/test/java/com/stingion/makeitfine/data/service/BankServiceTest.java
+++ mainside/src/test/java/com/stingion/makeitfine/data/service/BankServiceTest.java
@@ -1,21 +1,6 @@
 package com.stingion.makeitfine.data.service;
 
 import com.stingion.makeitfine.data.model.Bank;
-import org.junit.Ignore;
-import org.junit.Test;
 
-import javax.persistence.CascadeType;
-import javax.persistence.OneToMany;
-
-//todo: add testing of user_profile many-to-many relation
 public class BankServiceTest extends EntityServiceTestAncestor<Bank> {
-
-    @Ignore//todo: should be fixed (not ignore)
-    //Watch cascade deletion in related entities (bank, credit_card, order etc;
-//    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-    @Test
-    @Override
-    public void deleteTest() {
-        super.deleteTest();
-    }
 }
