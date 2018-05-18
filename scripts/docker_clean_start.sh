9c9ba42d6030925d15333cb9b70954771e34ab09
imalysh <imalysh@luxoft.com>
2018-05-18 21:05:26 +0300

    Copy sources in other container dir && write script for docker clean_start

---
+++ scripts/docker_clean_start.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# Main docker start script
+#
+echo "========================================================="
+echo " Remove docker container, create new ones and start them"
+echo "========================================================="
+echo ""
+docker stop mys j8m
+docker rm mys j8m
+docker network rm makeitfine_mifnet_back
+docker-compose up -d
+wait_time=15s
+echo "sleep for $wait_time while maven repos download executes"
+sleep $wait_time
+docker exec -it j8m bash '/data/output.sh'
