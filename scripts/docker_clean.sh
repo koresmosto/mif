11e243e6ba906256f5df921244fac74a541412d8
stingion <stingion@gmail.com>
2019-11-12 10:59:00 +0200

    Fixes after docker config j8m to j11m (Java 8 -> Java 11)

--- scripts/docker_clean.sh
+++ scripts/docker_clean.sh
@@ -5,7 +5,7 @@
 echo "=========================="
 echo " Remove docker containers"
 echo "=========================="
-echo ""
+echo "docker stop mys j11m:"
 docker stop mys j11m
+echo "docker rm mys j11m:"
 docker rm mys j11m
-docker network rm makeitfine_mifnet_back
--- scripts/docker_clean_start.sh
+++ scripts/docker_clean_start.sh
@@ -2,15 +2,14 @@
 #
 # Main docker start script
 #
+# Run from local machine: pwd && m clean install && c mainside/ && m spring-boot:run
+#
 echo "=========================================================="
 echo " Remove docker containers, create new ones and start them"
 echo "=========================================================="
-echo ""
-docker stop mys j11m
-docker rm mys j11m
-docker network rm makeitfine_mifnet_back
+. $(dirname $0)/docker_clean.sh
 docker-compose up -d
-wait_time=10s
-echo "sleep for $wait_time while maven repos download executes"
+wait_time=2s
+echo "sleep for $wait_time for 'echo 'tail -f /data/log.txt' >> output.sh' executes in container"
 sleep $wait_time
 docker exec -it j11m bash '/data/output.sh'
