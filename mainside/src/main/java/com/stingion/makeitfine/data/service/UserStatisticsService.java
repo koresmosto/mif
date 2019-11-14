cc62bd4b41bdbbf43a0027461856ccf420a8edd8
stingion <stingion@gmail.com>
2019-11-14 09:59:48 +0200

    Define UserStatisticsService interface

---
+++ mainside/src/main/java/com/stingion/makeitfine/data/service/UserStatisticsService.java
@@ -0,0 +1,40 @@
+/*
+ * Created under not commercial project "Make it fine"
+ *
+ *  Copyright 2017-2019
+ *  @author stingion
+ */
+
+package com.stingion.makeitfine.data.service;
+
+import com.stingion.makeitfine.data.model.user.User;
+import com.stingion.makeitfine.data.model.utils.State;
+import com.stingion.makeitfine.data.model.utils.UserProfileType;
+
+import java.util.List;
+
+public interface UserStatisticsService {
+  /**
+   * Get list of users of specified email host (e.g gmail.com, yahoo.com, ukr.net)
+   *
+   * @param emailHost - email host (e.g. gmail.com)
+   * @return users
+   */
+  List<User> specifiedMailServiceUsers(String emailHost);
+
+  /**
+   * Get list of users of specified type
+   *
+   * @param userProfileType profile type
+   * @return users
+   */
+  List<User> roleUsers(UserProfileType userProfileType);
+
+  /**
+   * Get list of users of specified state
+   *
+   * @param userState state
+   * @return users
+   */
+  List<User> stateUsers(State userState);
+}
