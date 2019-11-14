d71959277a1d4b979941326f749ea0c14d313d69
stingion <stingion@gmail.com>
2019-11-14 10:08:38 +0200

    Define UserStatisticsServiceImpl class

---
+++ mainside/src/main/java/com/stingion/makeitfine/data/service/impl/UserStatisticsServiceImpl.java
@@ -0,0 +1,33 @@
+/*
+ * Created under not commercial project "Make it fine"
+ *
+ *  Copyright 2017-2019
+ *  @author stingion
+ */
+
+package com.stingion.makeitfine.data.service.impl;
+
+import com.stingion.makeitfine.data.model.user.User;
+import com.stingion.makeitfine.data.model.utils.State;
+import com.stingion.makeitfine.data.model.utils.UserProfileType;
+import com.stingion.makeitfine.data.service.UserStatisticsService;
+import org.apache.commons.lang3.NotImplementedException;
+
+import java.util.List;
+
+public class UserStatisticsServiceImpl implements UserStatisticsService {
+    @Override
+    public List<User> specifiedMailServiceUsers(String emailHost) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<User> roleUsers(UserProfileType userProfileType) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<User> stateUsers(State userState) {
+        throw new UnsupportedOperationException();
+    }
+}
