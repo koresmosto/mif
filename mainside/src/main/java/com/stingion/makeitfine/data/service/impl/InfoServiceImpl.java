ace8928ed754825f2a5a8937618cc6024d801fbc
stingion <stingion@gmail.com>
2019-11-15 11:43:34 +0200

    Impl. usersAndItsRoles in InfoService

--- mainside/src/main/java/com/stingion/makeitfine/data/service/impl/InfoServiceImpl.java
+++ mainside/src/main/java/com/stingion/makeitfine/data/service/impl/InfoServiceImpl.java
@@ -7,15 +7,31 @@
 
 package com.stingion.makeitfine.data.service.impl;
 
+import com.stingion.makeitfine.data.model.user.User;
 import com.stingion.makeitfine.data.service.InfoService;
+import com.stingion.makeitfine.data.service.UserService;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class InfoServiceImpl implements InfoService {
 
+  @Autowired
+  private UserService userService;
+
   @Override
   public List<String> usersAndItsRoles() {
-    throw new UnsupportedOperationException();
+    return userService.findAll().stream()
+        .sorted(Comparator.comparing(User::getSsoId))
+        .map(user -> user.getSsoId().toLowerCase() + " : { " +
+            Optional.ofNullable(user.getUserProfiles())
+                .flatMap(userProfiles -> userProfiles.stream().map(u -> u.getType())
+                    .reduce((t1, t2) -> t1 + ", " + t2)).orElse("")
+            + " }")
+        .collect(Collectors.toList());
   }
 }
--- mainside/src/test/java/com/stingion/makeitfine/data/service/InfoServiceIT.java
+++ mainside/src/test/java/com/stingion/makeitfine/data/service/InfoServiceIT.java
@@ -7,6 +7,11 @@
 
 package com.stingion.makeitfine.data.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.google.common.collect.Lists;
+import java.util.List;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,4 +27,21 @@ class InfoServiceIT {
 
   @Autowired
   private InfoService infoService;
+
+  @Test
+  void usersAndItsRoles() {
+    List<String> expected = Lists.newArrayList(
+        "admin : { ADMIN }",
+        "bill : { USER }",
+        "danny : { USER }",
+        "kenny : { ADMIN, DBA }",
+        "nicole : { DBA }",
+        "sam : { ADMIN }",
+        "sudo : { USER, ADMIN, DBA }",
+        "tsuser : { USER }"
+    );
+
+    assertEquals(expected, infoService.usersAndItsRoles());
+    infoService.usersAndItsRoles();
+  }
 }
