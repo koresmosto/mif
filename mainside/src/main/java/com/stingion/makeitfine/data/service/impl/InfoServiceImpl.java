96b2bd2e5aa12787873145160d33ef293197eec7
stingion <stingion@gmail.com>
2019-11-15 09:30:07 +0200

    Define InfoService

---
+++ mainside/src/main/java/com/stingion/makeitfine/data/service/impl/InfoServiceImpl.java
@@ -0,0 +1,16 @@
+/*
+ * Created under not commercial project "Make it fine"
+ *
+ *  Copyright 2017-2019
+ *  @author stingion
+ */
+
+package com.stingion.makeitfine.data.service.impl;
+
+import com.stingion.makeitfine.data.service.InfoService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InfoServiceImpl implements InfoService {
+
+}
---
+++ mainside/src/test/java/com/stingion/makeitfine/data/service/InfoServiceIT.java
@@ -0,0 +1,25 @@
+/*
+ * Created under not commercial project "Make it fine"
+ *
+ *  Copyright 2017-2019
+ *  @author stingion
+ */
+
+package com.stingion.makeitfine.data.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@ActiveProfiles("integration_test")
+@Transactional
+class InfoServiceIT {
+
+  @Autowired
+  private InfoService infoService;
+}
