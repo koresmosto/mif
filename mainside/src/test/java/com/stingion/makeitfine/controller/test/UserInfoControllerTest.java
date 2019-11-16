b361344d7bd14a93e16015d2fe00c6a9fb9ad94a
stingion <stingion@gmail.com>
2019-11-16 18:50:58 +0200

    Add UserInfoControllerTest

---
+++ mainside/src/test/java/com/stingion/makeitfine/controller/test/UserInfoControllerTest.java
@@ -0,0 +1,58 @@
+/*
+ * Created under not commercial project "Make it fine"
+ *
+ *  Copyright 2017-2019
+ *  @author stingion
+ */
+
+package com.stingion.makeitfine.controller.test;
+
+import static org.mockito.Mockito.when;
+
+import com.google.common.collect.Lists;
+import com.stingion.makeitfine.controller.UserInfoController;
+import com.stingion.makeitfine.data.service.InfoService;
+import java.util.ArrayList;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@ExtendWith(SpringExtension.class)
+@WebFluxTest(controllers = UserInfoController.class)
+@Tag("controllerTest")
+class UserInfoControllerTest {
+
+  @Autowired
+  private WebTestClient webTestClient;
+
+  @MockBean
+  private InfoService infoService;
+
+  ArrayList<String> usersAndItsRoles = Lists
+      .newArrayList("user1:{USER,ADMIN,DBA}", "user2:{}", "user3:{USER,ADMIN,DBA}");
+
+  @BeforeEach
+  void init() {
+    when(infoService.usersAndItsRoles()).thenReturn(usersAndItsRoles);
+  }
+
+  @WithMockUser
+  @Test
+  void usersAndItsRoles() {
+    webTestClient.get()
+        .uri("/userInfo/usersAndItsRoles")
+        .exchange()
+        .expectStatus().isOk()
+        .expectBody(String.class)
+        .value(
+            CoreMatchers.equalTo(usersAndItsRoles.stream().reduce((e1, e2) -> e1 + "" + e2).get()));
+  }
+}
