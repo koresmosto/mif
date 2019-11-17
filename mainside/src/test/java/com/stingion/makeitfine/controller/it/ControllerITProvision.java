7f3e51c54c37da7c7643f3dccf385d6ca2346513
stingion <stingion@gmail.com>
2019-11-17 12:33:24 +0200

    controller itests fixed

--- mainside/src/test/java/com/stingion/makeitfine/controller/it/ControllerITProvision.java
+++ mainside/src/test/java/com/stingion/makeitfine/controller/it/ControllerITProvision.java
@@ -9,20 +9,16 @@ package com.stingion.makeitfine.controller.it;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+import com.stingion.makeitfine.testconfiguration.IntegrationTest;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-@ActiveProfiles("integration_test")
-@TestPropertySource("classpath:values-test.yml")
-@ConfigurationProperties(prefix = "test.integration")
+@IntegrationTest
 abstract class ControllerITProvision {
 
   @LocalServerPort
---
+++ mainside/src/test/java/com/stingion/makeitfine/testconfiguration/IntegrationTest.java
@@ -0,0 +1,34 @@
+/*
+ * Created under not commercial project "Make it fine"
+ *
+ *  Copyright 2017-2019
+ *  @author stingion
+ */
+
+package com.stingion.makeitfine.testconfiguration;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+/**
+ * User to run unit tests
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@ActiveProfiles("integration_test")
+@TestPropertySource("classpath:values-test.yml")
+@ConfigurationProperties(prefix = "test.integration")
+@WithMockUser
+public @interface IntegrationTest {
+
+}
