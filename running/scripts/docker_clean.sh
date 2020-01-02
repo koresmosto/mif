2a4cf03108291825d1ae669760d6536535c3cb40
stingion <stingion@gmail.com>
2020-01-02 17:55:02 +0200

    Fix docker-compose commands

--- running/scripts/docker_clean.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo "=================================="
-echo " Remove docker containers locally "
-echo "=================================="
-echo "docker stop mys j11m:"
-docker stop mys j11m
-echo "docker rm mys j11m:"
-docker rm mys j11m
