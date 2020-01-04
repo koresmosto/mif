6a0aa51f3bfa3b1de8aabcd16265e8c1a951819e
stingion <stingion@gmail.com>
2020-01-04 22:09:33 +0200

    Intro parametrized script for docker running

---
+++ running/scripts/docker/br.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+echo "===================================================================="
+echo " Build project and run docker PARAMETRIZED        "
+echo " Run in debug (d) skipTest (s), e.g.: br.sh s d | br.sh s | br.sh d "
+echo "===================================================================="
+
+DskipTests=""
+docker_compose_file="docker-compose.yml"
+
+for arg in "$@"
+do
+    case "$arg" in
+      "s")    DskipTests="-DskipTests=true"
+              ;;
+      "d")    docker_compose_file="docker-compose-debug.yml"
+              ;;
+      *)
+    esac
+done
+
+echo "Script running>> ${DskipTests} ${docker_compose_file}"
+
+PROJECT_PATH="`dirname \"$0\"`"/../../..
+
+mvn clean install ${DskipTests} -Pdocker -f ${PROJECT_PATH}
+docker-compose -f ${PROJECT_PATH}/${docker_compose_file} up
