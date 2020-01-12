8faa694977dbd61c25687ddac85fb86c47c48668
stingion <stingion@gmail.com>
2020-01-12 15:36:11 +0200

    Rename db-inti to db-migrate

--- run/dev/mongo_migrate.sh
+++ run/dev/mongo_migrate.sh
@@ -4,6 +4,6 @@ echo " Migrate MongoDB on local env. (dev)                                "
 echo "===================================================================="
 
 PROJECT_PATH="`dirname \"$0\"`"/../..
-MONGO_MIGRATION_SCRIPTS_PATH="${PROJECT_PATH}/run/mongodb/resources/db-init";
+MONGO_MIGRATION_SCRIPTS_PATH="${PROJECT_PATH}/run/mongodb/resources/db-migrate";
 
 mongo --port 27017 admin ${MONGO_MIGRATION_SCRIPTS_PATH}/*.js --authenticationDatabase "admin" -u "root" -p "rootpass"
