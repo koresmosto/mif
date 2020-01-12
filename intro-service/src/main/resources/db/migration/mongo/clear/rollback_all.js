4a9e6516bf6613d956bf74d07caa149828b9a7a2
stingion <stingion@gmail.com>
2020-01-12 17:17:11 +0200

    Add clear mongodb script

---
+++ intro-service/src/main/resources/db/migration/mongo/clear/rollback_all.js
@@ -0,0 +1,11 @@
+/*
+ * Created under not commercial project "Make it fine"
+ *
+ *  Copyright 2017-2020
+ *  @author stingion
+ */
+
+db.dropUser("introuser");
+
+db = db.getSiblingDB("introdb");
+db.dropDatabase();
--- run/dev/mongo_migrate.sh
+++ run/dev/mongo_migrate.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 echo "===================================================================="
 echo " Migrate MongoDB on local env. (dev)                                "
+echo " (It's clearing DB before migration)                                "
 echo "===================================================================="
 
 PROJECT_PATH="`dirname \"$0\"`"/../..
 MONGO_MIGRATION_SCRIPTS_PATH="${PROJECT_PATH}/intro-service/src/main/resources/db/migration/mongo";
 
-mongo --port 27017 admin ${MONGO_MIGRATION_SCRIPTS_PATH}/*.js --authenticationDatabase "admin" -u "root" -p "rootpass"
+# Clear DB before migration
+mongo --port 27017 admin ${MONGO_MIGRATION_SCRIPTS_PATH}/clear/*.js --authenticationDatabase "admin" -u "root" -p "rootpass"
+
+# Migrate
+mongo --port 27017 admin ${MONGO_MIGRATION_SCRIPTS_PATH}/init/*.js --authenticationDatabase "admin" -u "root" -p "rootpass"
