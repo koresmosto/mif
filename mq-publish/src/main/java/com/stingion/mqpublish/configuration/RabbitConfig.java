184e408176d7fe8ff3530a8742faac4a0f8147fc
stingion <stingion@gmail.com>
2020-01-15 13:51:58 +0200

    Config queue/exchange creation when app. starts

--- mq-publish/src/main/java/com/stingion/mqpublish/configuration/RabbitConfig.java
+++ mq-publish/src/main/java/com/stingion/mqpublish/configuration/RabbitConfig.java
@@ -7,6 +7,7 @@
 
 package com.stingion.mqpublish.configuration;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
@@ -14,9 +15,14 @@ import org.springframework.amqp.core.Exchange;
 import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Configuration
 public class RabbitConfig {
@@ -40,5 +46,22 @@ public class RabbitConfig {
   @Bean
   public Binding binding(Queue secretUrlQueue, Exchange secretUrlExchange) {
     return BindingBuilder.bind(secretUrlQueue).to((DirectExchange) secretUrlExchange).with("");
+  }
+
+  @Bean
+  public RabbitAdmin admin(ConnectionFactory cf) {
+    return new RabbitAdmin(cf);
+  }
+
+  @Component
+  @RequiredArgsConstructor
+  public class RabbitMqStartup implements ApplicationListener<ApplicationReadyEvent> {
+
+    private final RabbitAdmin rabbitAdmin;
+
+    @Override
+    public void onApplicationEvent(final ApplicationReadyEvent event) {
+      rabbitAdmin.initialize();
+    }
   }
 }
