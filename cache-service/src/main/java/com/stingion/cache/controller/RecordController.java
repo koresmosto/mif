8d2fc5b5aa9c3e4b04f849e7c75fde21ad7ae0fc
stingion <stingion@gmail.com>
2020-01-16 20:29:37 +0200

    Put and delete operations of Records in cache module

--- cache-service/src/main/java/com/stingion/cache/controller/RecordController.java
+++ cache-service/src/main/java/com/stingion/cache/controller/RecordController.java
@@ -15,10 +15,15 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,11 +52,34 @@ public class RecordController {
     return recordRepository.findById(Long.valueOf(recordId)).orElse(null);
   }
 
+  @CachePut(value = "records", key = "#record.id")
+  @PutMapping("/update")
+  public Record updateRecordByID(@RequestBody Record record) {
+    recordRepository.save(record);
+    return record;
+  }
+
+  /**
+   * Delete record. E. g.
+   * <pre>
+   * $>curl -H 'Content-Type: application/json' -X PUT -d '{"id":12,"msg":"Hi, there!!!"}'
+   * http://localhost:8084/records/update
+   * </pre>
+   * @param recordId id
+   */
+  @CacheEvict(value = "records", key = "#recordId")
+  @DeleteMapping("/{recordId}")
+  public void deleteRecordByID(@PathVariable Long recordId) {
+    log.info("deleting record with id {}", recordId);
+    recordRepository.deleteById(recordId);
+  }
+
   /**
    * Refresh all messages by getting them from queue.
    *
    * @return messages contained response
    */
+  @CacheEvict(value = "records", allEntries = true)
   @GetMapping("refresh")
   public ResponseEntity<List<Message>> hello() {
     String url =
