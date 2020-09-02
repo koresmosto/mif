2585b893e135951ad6f0ef08c0b0b99a7b3f9362
stingion <stingion@gmail.com>
2020-09-02 19:55:10 +0300

    Run testcontainers of yaypay module on Junit4

---
+++ yaypay/src/test/java/com/stingion/yaypay/data/repository/AnyRepositoryTest.java
@@ -0,0 +1,60 @@
+/*
+ * Created under not commercial project "Make it fine"
+ *
+ * Copyright 2017-2020
+ */
+
+package com.stingion.yaypay.data.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Collections;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+@ContextConfiguration(initializers = AnyRepositoryTest.Initializer.class)
+public class AnyRepositoryTest {
+
+    @Container
+    private static final MySQLContainer<?> MY_SQL_CONTAINER = new MySQLContainer<>()
+            .withDatabaseName("db")
+            .withUsername("sa")
+            .withPassword("sa");
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues values = TestPropertyValues.of(
+                    "spring.datasource.url=" + MY_SQL_CONTAINER.getJdbcUrl(),
+                    "spring.datasource.password=" + MY_SQL_CONTAINER.getPassword(),
+                    "spring.datasource.username=" + MY_SQL_CONTAINER.getUsername(),
+                    "spring.datasource.driver-class-name=" + MY_SQL_CONTAINER.getDriverClassName()
+            );
+            values.applyTo(configurableApplicationContext);
+        }
+    }
+
+    @Test
+    public void test() {
+        assertTrue(MY_SQL_CONTAINER.isRunning());
+        assertEquals(2, jdbcTemplate.queryForList("show databases").size());
+        assertThat(jdbcTemplate.queryForList("show tables")).isEqualTo(Collections.EMPTY_LIST);
+    }
+}
